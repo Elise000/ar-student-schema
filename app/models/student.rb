804dbb463d92fea21f65b319731f6ebda7e44ea6
wengkhing <kheryee@live.com>
2015-10-15 19:40:14 +0800

    validation magic and advance validations

--- app/models/student.rb
+++ app/models/student.rb
@@ -3,6 +3,24 @@ require_relative '../../db/config'
 class Student < ActiveRecord::Base
 
 attr_accessor :first_name, :last_name, :birthday
+
+  # validates :first_name, :presence => true
+
+  validates :email, :format => { :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/, :message => "error"}
+
+  validates :email, :uniqueness => true
+
+  validates :age, :numericality => {:greater_than => 4}
+
+  validates :phone, :format => { :with => /\D\d{3}\D{2}\d{3}\D\d{4}\D{2}\d{4}/, :message => "error phone"}
+
+# validates :email, :format => { :with => /[a-z]*@[a-z]*.[a-z]{2,}/, :message => "error"}
+
+#     validates :email, uniqueness: true
+
+#     validates :age, :numericality => {:greater_than => 5
+#     }
+
   def name
     self.first_name + " " + self.last_name
   end
