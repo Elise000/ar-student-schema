117c8490b2ffdd1426e5cd7aca63653bcdce2743
wengkhing <kheryee@live.com>
2015-10-15 18:49:50 +0800

    def name and age method for Student model

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,5 +1,17 @@
 require_relative '../../db/config'
 
 class Student < ActiveRecord::Base
+
+attr_accessor :first_name, :last_name, :birthday
+  def name
+    self.first_name + " " + self.last_name
+  end
+
+  def age
+    now = Date.today
+    age = now.year - self.birthday.year
+    # - ((now.month > self.birthday.month || (now.month == self.birthday.month && now.day >= self.birthday.day)) ? 0 : 1)
+    return age
+  end
 # implement your Student model here
 end
